2a4265dca34357a4db4ca5b4eb82e0bc64572d1a
JasonM0712 <155554008+JasonM0712@users.noreply.github.com>
2024-04-22 12:07:16 +0200

    JasonUpdate 22/04

---
+++ src/Controller/BibliothequeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\SearchData;
+use App\Form\SearchType;
+use App\Repository\LivresRepository;
+use App\Repository\AuteursRepository;
+use App\Repository\EtatsLivresRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BibliothequeController extends AbstractController
+{
+    #[Route('/bibliotheque', name: 'app_bibliotheque')]
+    public function index(LivresRepository $livreRepository, AuteursRepository $AuteursRepository, SearchData $data, Request $request, EtatsLivresRepository $etatsLivres ): Response
+    {
+       
+        $etatsLivresData = $etatsLivres->findAll();
+        $form = $this->createForm(SearchType::class, $data);
+        $form ->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $livres = $livreRepository->search($data);
+            dd($livres);
+          
+            return $this->render('bibliotheque/index.html.twig', [
+                'controller_name' => 'BibliothequeController',
+                'livres' => $livres,
+                'auteurs' => $AuteursRepository->findAll(),
+                'form' => $form->createView(),
+                'etatsLivres' => $etatsLivresData
+            ]);
+        }
+
+        return $this->render('bibliotheque/index.html.twig', [
+            'controller_name' => 'BibliothequeController',
+            'livres' => $livreRepository->findAll(),
+            'auteurs' => $AuteursRepository->findAll(),
+            'form' => $form->createView(),
+            'etatsLivres' => $etatsLivresData
+
+        ]);
+
+
+
+    }
+    public function show($livreId, LivresRepository $livreRepository, EtatsLivresRepository $EtatRepo): Response
+    {
+        $livreEtat = $livreRepository->find($livreId);
+
+        if(!$livreEtat){
+            throw $this->createNotFoundException('Le livre n\'existe pas');
+        }
+        $etatsLivres = $livreEtat->getEtatsLivres();
+
+        return $this->render('bibliotheque/show.html.twig', [
+            'livreEtat' => $livreEtat
+        ]);
+
+
+    }
+}
